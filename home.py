566f81ea358af7e24a83581abe3b452ff990554b
yadugit <74152475+yadugit@users.noreply.github.com>
2025-05-13 21:53:54 +0530

    Update home.py

--- home.py
+++ home.py
@@ -10,13 +10,6 @@ def home_page():
     # Embed video
     # Embed Loom video
     st.markdown("""
-    <div style="position: relative; padding-bottom: 64.98194945848375%; height: 0;">
-        <iframe src="https://www.loom.com/share/935407b16d2d437eb8102005406ace29?sid=8a8334b0-e987-475c-8a34-6d5d92455034" 
-                frameborder="0" 
-                webkitallowfullscreen 
-                mozallowfullscreen 
-                allowfullscreen 
-                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;">
-        </iframe>
-    </div>
-    """, unsafe_allow_html=True)
+    ### 📹 Demo Video
+    [Click here to watch the Loom video](https://www.loom.com/share/935407b16d2d437eb8102005406ace29?sid=8a8334b0-e987-475c-8a34-6d5d92455034)
+    """)
